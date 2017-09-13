bda9a60ef4d999e6163cf33c9f4bedfea195d9dc
cho881020 <cho881020@gmail.com>
2017-09-13 10:34:51 +0900

    블루투스 기기 탐색 기능

--- app/src/main/java/kr/co/tjeit/bluetoothpracetice/DeviceListActivity.java
+++ app/src/main/java/kr/co/tjeit/bluetoothpracetice/DeviceListActivity.java
@@ -1,10 +1,21 @@
 package kr.co.tjeit.bluetoothpracetice;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.tjeit.bluetoothpracetice.adapter.BtAdapter;
+import kr.co.tjeit.bluetoothpracetice.data.BtDevice;
 
 public class DeviceListActivity extends BaseActivity {
 
@@ -12,6 +23,9 @@ public class DeviceListActivity extends BaseActivity {
     private android.widget.Button scanBtn;
     private android.widget.ListView newDeviceListView;
     private android.widget.ListView pairedDeviceListView;
+
+    List<BtDevice> newDeviceList = new ArrayList<>();
+    BtAdapter mBtListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,9 +74,62 @@ public class DeviceListActivity extends BaseActivity {
     @Override
     public void setValues() {
 
+//        탐색된 블루투스 기기를 보여줄 리스트뷰 세팅
 
+        mBtListAdapter = new BtAdapter(mContext, newDeviceList);
+        newDeviceListView.setAdapter(mBtListAdapter);
+
+
+//        브로드 캐스트 리시버를 등록.
+
+//        수신하고싶은 방송의 종류를 설정.
+//        1. 블루투스 기기를 찾았다! 라는 방송을 받겠다.
+        IntentFilter foundFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+
+//        수신하고 싶은 방송을 등록
+//        재료 2가지.
+//        1) 방송이 수신되었을때 진행할 행동을 담은 Receiver => 맨 밑에 따로 작성
+//        2) 어떤 방송을 수신할지 설정해둔 IntentFilter
+        registerReceiver(mReceiver, foundFilter);
 
     }
+
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+//            블루투스 기기를 찾았을때 수신되는 신호를 가지고
+//            이벤트 처리를 해준다.
+
+//            어떤 방송이 수신되었나?
+            String actionName = intent.getAction();
+
+//            1. 기기 탐색(Discovering) 결과 어떤 기기를 발견했을때.
+
+            if (actionName.equals(BluetoothDevice.ACTION_FOUND)) {
+
+//                방송 데이터 안에 들어있는 블루투스기기 클래스를 받아오기.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+//                찾아낸 기기가, 이미 페어링 된 적이 있다면 무시.
+//                새 장비가 아니니, 굳이 페어링 가능 목록에 띄워줄 필요가 없다.
+
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+//                    아직 연결된 적이 없는 기기.
+
+                    newDeviceList.add(new BtDevice(device.getName(), device.getAddress()));
+                    mBtListAdapter.notifyDataSetChanged();
+
+                }
+
+
+            }
+
+
+        }
+    };
+
 
     @Override
     public void bindViews() {
---
+++ app/src/main/java/kr/co/tjeit/bluetoothpracetice/adapter/BtAdapter.java
@@ -0,0 +1,46 @@
+package kr.co.tjeit.bluetoothpracetice.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import kr.co.tjeit.bluetoothpracetice.R;
+import kr.co.tjeit.bluetoothpracetice.data.BtDevice;
+
+/**
+ * Created by user on 2017-09-13.
+ */
+
+public class BtAdapter extends ArrayAdapter<BtDevice> {
+
+    Context mContext;
+    List<BtDevice> mList;
+    LayoutInflater inf;
+
+    public BtAdapter(Context context, List<BtDevice> list) {
+        super(context, R.layout.btdevice_list_item, list);
+
+        mContext = context;
+        mList = list;
+        inf = LayoutInflater.from(mContext);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+        if (row == null) {
+            row = inf.inflate(R.layout.btdevice_list_item, null);
+        }
+        return row;
+    }
+}
+
+
